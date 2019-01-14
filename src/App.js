03f3393fba602703b6943a7d8524d3eae37fa195
ranguro <randy1002@gmail.com>
2019-01-14 07:39:39 -0600

    feat: added searchAppBar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
+import axios from 'axios';
 import React, { Component } from "react";
 import "./App.css";
+import SearchAppBar from './components/SearchAppBar';
 
 class App extends Component {
+
+
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
   render() {
     return (
-      <div
-        style={{
-          alignSelf: "center",
-          display: "flex"
-        }}
-      >
-        <code>
-          TODO: build single page application that searches a photo API and
-          displays the results
-        </code>
-      </div>
+      <SearchAppBar title='GIF Search' searchPlaceholder='Search all GIFs' onSearch={this.handleSearch} />
     );
+  }
+
+  handleSearch(input) {
+    const API_URL = 'http://api.giphy.com/v1/gifs/search';
+    const API_KEY = 'LiT4XaeBUDCDtVpLNuTcc8fzKv84AmW6';
+    axios.get(`${API_URL}?q=${input.trim().replace(/ /g, "+")}&api_key=${API_KEY}`)
+      .then(res => {
+        const data = res.data.data;
+        this.setState({ data });
+      })
   }
 }
 
